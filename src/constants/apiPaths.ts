06ff49e12c8c177d6bbf30bc3906df8179605eb5
Net-zen <prankster@myttk.ru>
2021-10-25 12:01:45 +0600

    feat: replace cart api url with apigateway proxy for cart api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://nt48g1u0ak.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://i5f9ybk28g.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://net-zen-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://osifsctd4d.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
