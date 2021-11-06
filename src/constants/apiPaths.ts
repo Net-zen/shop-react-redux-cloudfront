c0b4be23884d9cf443bda39293dec7611a1f8482
Net-zen <prankster@myttk.ru>
2021-11-06 16:19:04 +0600

    feat: add new bff path & fix product path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://i5f9ybk28g.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://nt48g1u0ak.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://i5f9ybk28g.execute-api.eu-west-1.amazonaws.com/dev',
